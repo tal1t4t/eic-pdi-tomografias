c89a479d03ba8ecd11d85007a8d7431225abd008
Talita Toledo <talitatoledo3@gmail.com>
2026-07-01 01:21:48 -0300

    removedor de fundo

---
+++ remove_fundo.py
@@ -0,0 +1,12 @@
+from utils import in_file, out_file
+from rembg import remove
+from PIL import Image
+from otsu import threshold_image, find_best_threshold, abre_imagem, salva_imagem_otsu
+
+original = in_file('img2.png')
+sem_fundo = 'img2_semfundo.png'
+
+input = Image.open(original)
+output = remove(input)
+
+output.save(out_file(sem_fundo))
