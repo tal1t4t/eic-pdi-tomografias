b471dc87a02e8b32032f92395172de45ce3eb522
Talita Toledo <talitatoledo3@gmail.com>
2026-07-06 16:16:52 -0300

    removedor de fundo com openCV

--- remove_fundo.py
+++ remove_fundo.py
@@ -2,16 +2,26 @@
 
 from utils import in_file, out_file
 from rembg import remove
-from PIL import Image
-# from otsu import threshold_image, find_best_threshold, abre_imagem, salva_imagem_otsu
+# from PIL import Image
+import cv2
 
+# def remove_fundo(img):
+#     original = Image.open(in_file(img))
+#     output = remove(original)
+#     output.save(out_file(f"semfundo_{img}"))
+    
+#     return output
 
-def remove_fundo(img):
-    original = Image.open(in_file(img))
-    output = remove(original)
-    output.save(out_file(f"semfundo_{img}"))
+def remove_fundo(nome_img):
+    #imread retorna uma narray do numpy
+    original = cv2.imread(in_file(nome_img) + '.png')
+
+    semfundo = remove(original)
+    cv2.imwrite(out_file(f'semfundo_{nome_img}.png'), semfundo)
+
+    return semfundo
 
 
 if __name__ == "__main__":
-    original = 'img2.png'
-    remove_fundo(original)
+    nome_original = 'img2'
+    remove_fundo(nome_original)
