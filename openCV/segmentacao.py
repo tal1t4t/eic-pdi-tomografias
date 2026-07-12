e727a8bd4f10f03306f8d646d8570a7f69ca811b
Talita Toledo <talitatoledo3@gmail.com>
2026-07-12 13:20:37 -0300

    aplicando operadores morfológicos

--- openCV/segmentacao.py
+++ openCV/segmentacao.py
@@ -5,54 +5,59 @@ import tkinter as tk
 from tkinter import filedialog, ttk
 import matplotlib.pyplot as plt
 
-def mostra_valores():
-    print(f"lower: {t_lower.get()}\nupper: {t_upper.get()}")
-    window.destroy()
+def fecha_janela():
+    janela.destroy()
+
+def atualizar(x):
+    inf = cv2.getTrackbarPos('Inferior', 'Controle de Bordas')
+    sup = cv2.getTrackbarPos('Superior', 'Controle de Bordas')
+
+    img_canny = cv2.Canny(img, inf, sup)
+
+    #dilatação de bordas
+    kernel = np.ones((3,3), np.uint8)
+    # dilated = cv2.dilate(img_canny, kernel, iterations=1)
+
+    # operação de abertura
+    # opening = cv2.morphologyEx(img_canny, cv2.MORPH_OPEN, kernel)
+    
+    # operação de fechamento
+    # closed = cv2.morphologyEx(img_canny, cv2.MORPH_CLOSE, kernel)
+
+    # legenda para a imagem com as bordas de canny
+    leg_inf = f'Inferior: {inf}'
+    cv2.putText(img_canny, leg_inf, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.6 ,(255, 255, 255), 1)
+
+    leg_sup = f'Superior: {sup}'
+    cv2.putText(img_canny, leg_sup, (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 1)
+
+
+    cv2.imshow('Controle de Bordas', img_canny)
+    # cv2.imshow('Bordas dilatadas', dilated)
+    # cv2.imshow('Abertura da imagem', opening)
+    cv2.imshow('Fechamento da imagem', closed)
+
 
 
 arquivo = filedialog.askopenfilename() # pega o caminho do arquivo
 img = cv2.imread(arquivo, cv2.IMREAD_GRAYSCALE)
 
 # ------------- Criação da janela interativa para inserir thresholds: ---------------
-janela = tk.Tk()
-janela.title("Inserção dos valroes para threshold")
 
-# Entry e Label para o t_lower:
-t_lower = tk.IntVar()
-label_t_lower = ttk.Label(janela, text="Lower Threshold:")
-label_t_lower.grid(row=0, column=0, padx=10, pady=5, sticky="e")
+cv2.namedWindow("Controle de Bordas")
 
-entry_t_lower = ttk.Entry(janela, textvariable=t_lower)
-entry_t_lower.grid(row=0, column=1, padx=10, pady=5)
-
-# Entry e Label para o t_upper:
-t_upper = tk.IntVar()
-label_t_upper = ttk.Label(janela, text="Upper Threshold:")
-label_t_upper.grid(row=1, column=0, padx=10, pady=5, sticky="e")
-
-entry_t_upper = ttk.Entry(janela, textvariable=t_upper)
-entry_t_upper.grid(row=1, column=1, padx=10, pady=5)
-
-# Botão para armazenar os valores
-botao_get = ttk.Button(janela, text="Enviar", command=mostra_valores)
-botao_get.grid(row=2, columnspan=2, padx=10, pady=10)
-
-janela.mainloop()
+cv2.createTrackbar("Inferior", "Controle de Bordas", 0, 255, atualizar)
+cv2.createTrackbar("Superior", "Controle de Bordas", 0, 255, atualizar)
 
 #-------execução das bordas de canny com base nos valores dados----------
+atualizar(0)
 
-t_lower, t_upper = janela_thresholds()
+# kernel = np.ones((3,3), np.uint8)
 
-img_canny = cv2.Canny(img, t_lower.get(), t_upper.get())
-#fzr comando pra salvar imagem canny de acordo com os valores dos thresholds!
-
-kernel = np.ones((5,5), np.uint8)
-
-erosion = cv2.erode(img, kernel, iterations=1)
+# dilated = cv2.dilate(img_canny, kernel, iterations=1)
 
 cv2.imshow('original', img)
-cv2.imshow('bordas de canny', img_canny)
-cv2.imshow('erosão', erosion)
+# cv2.imshow('dilatação', dilated)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
